3a922b980e4a5006850e2c718a8273430d91eca2
Novvea <anna.c.novak@gmail.com>
2021-03-08 14:10:59 +0100

    Create schema for user

---
+++ src/models/User.model.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose' //vår ODM
+
+const { Schema, model } = mongoose
+
+const userSchema = Schema({ //strukturera upp hur metadatan ska se ut som ska till databasen
+  username: { //här kan vi lägga till flera regler
+    type: String,
+    unique: true, //mongooses egen funktion, kollar att alla användarnamn är unika
+    allowNull: false,
+    required: true //anropet går inte igenom om vi inte skickar med ett användarnamn
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    min: [18, 'You need to be above 18'],
+    max: [123, 'You have the world record in age, please leave this site and go celebrate instead!'],
+    required: true
+  }
+}, { timestamps: true }) //får reda på när data sparades och uppdaterades
+
+const UserModel = model('user', userSchema)
+export default UserModel
