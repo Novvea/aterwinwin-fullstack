1c0ec2aec5de20f60eb14c135c44513d76e8b1a1
Novvea <anna.c.novak@gmail.com>
2021-03-09 10:23:37 +0100

    Add userroute for getAllUsers

--- src/routes/User.route.js
+++ src/routes/User.route.js
@@ -2,6 +2,7 @@ import UserController from '../controllers/User.controller.js'
 
 const routes = (application) => {
   application.post('/user', UserController.createUser)
+  application.get('/user', UserController.getAllUsers)
 }
 
 export default { routes }
