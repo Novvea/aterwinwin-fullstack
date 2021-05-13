c2e398175f93bf20b0dd0142a6c01ea39cb92436
Anna Novak <anna.c.novak@gmail.com>
2021-05-13 09:08:58 +0200

    Refactor to async-await

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,11 +1,8 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-export const fetchUser = () => {
-  return function (dispatch) {
-    console.log('[fetchUser] action called');
-    axios
-      .get('/api/current_user')
-      .then((response) => dispatch({ type: FETCH_USER, payload: response }));
-  };
+export const fetchUser = () => async (dispatch) => {
+  const response = await axios.get('/api/current_user');
+
+  dispatch({ type: FETCH_USER, payload: response.data });
 };
