f04acc4cf0dba7680728b9c4f46ad7c2ed9420c1
Anna Novak <anna.c.novak@gmail.com>
2021-05-13 10:49:21 +0200

    Add switch-case for rendering content in Header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,12 +6,32 @@ const Header = () => {
   const auth = useSelector((state) => state.auth);
   console.log('auth: ', auth);
 
+  function RenderContent(props) {
+    switch (props.value) {
+      case null:
+        return null; //BORDE FIXA DETTA!!!
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login with Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a>Logout</a>
+          </li>
+        );
+    }
+  }
+
   return (
     <nav>
       <a>This is the Header</a>
       <ul>
         <li>
-          <a>Login With Google</a>
+          {/* <a>Login With Google</a> */}
+          <RenderContent value={auth} />
         </li>
       </ul>
     </nav>
