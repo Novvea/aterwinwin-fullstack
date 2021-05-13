9241d5aad73802ab2f0145dc3f82111ad52fd24f
Anna Novak <anna.c.novak@gmail.com>
2021-05-13 10:16:28 +0200

    Get auth-status to Header-component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,4 +1,11 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 const Header = () => {
+  //const dispatch = useDispatch();
+  const auth = useSelector((state) => state.auth);
+  console.log('auth: ', auth);
+
   return (
     <nav>
       <a>This is the Header</a>
