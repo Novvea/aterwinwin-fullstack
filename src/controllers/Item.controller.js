170be8e7707adbd9a314c81968f37174bb142729
Anna Novak <anna.c.novak@gmail.com>
2021-05-20 16:57:10 +0200

    Adjust item schema and model

--- src/controllers/Item.controller.js
+++ src/controllers/Item.controller.js
@@ -1,95 +1,107 @@
 //här skriver vi logiken som talar om vad som händer när vi gör ett anrop
-const ItemModel = require('../models/Item.model')
+const ItemModel = require('../models/Item.model');
 
 //async await pga att mongoose måste koppla upp sig tyill databasen, vi väntar tills processen är färdig innan vi gör något annat
-const addItem = async (request, response) => { //request = den data som skickas till anropet, response är det servern svarar med
-
+const addItem = async (request, response) => {
+  //request = den data som skickas till anropet, response är det servern svarar med
   const item = new ItemModel({
-    name: request.body.name,//ska peka på den data som ska skickas till servern
+    name: request.body.name, //ska peka på den data som ska skickas till servern
     category: request.body.category,
+    position: request.body.position,
     url: request.body.url,
-    owner: request.body.owner,
+    _user: request.body.id, //the id comes from mongoose and mongodb
     interestedUsers: request.body.interestedUsers,
-    unInterestedUsers: request.body.unInterestedUsers
-  })
-
+    unInterestedUsers: request.body.unInterestedUsers,
+  });
   try {
-    const databaseResponse = await item.save() //spara data som skickas till servern i databasen
-    response.status(201).send(databaseResponse)//om det går som planerat vill vi tala om det, vi skickar tillbaka ett svar från servern och datan
-  } catch (error) {
-    response.status(500).send({ //men nådde i alla fall fram till servern
-      message: 'Error while trying to add item',
-      stack: error
-    })
-  }
-}
-const getAllItems = async (request, response) => {
-  try {
-    const databaseResponse = await ItemModel.find()
-    response.status(200).send(databaseResponse)
+    const databaseResponse = await item.save(); //spara data som skickas till servern i databasen
+    response.status(201).send(databaseResponse); //om det går som planerat vill vi tala om det, vi skickar tillbaka ett svar från servern och datan
   } catch (error) {
     response.status(500).send({
-      message: error.message
-    })
+      //men nådde i alla fall fram till servern
+      message: 'Error while trying to add item',
+      stack: error,
+    });
   }
-}
+};
+
+const getAllItems = async (request, response) => {
+  try {
+    const databaseResponse = await ItemModel.find();
+    response.status(200).send(databaseResponse);
+  } catch (error) {
+    response.status(500).send({
+      message: error.message,
+    });
+  }
+};
 
 const deleteItem = async (request, response) => {
   try {
-    const itemId = request.params.itemId
-    const databaseResponse = await ItemModel.findByIdAndDelete(itemId)//ska hitta en vara baserat på id och sedan deleta
-    response.status(200).send({ message: 'Successfully deleted item', data: databaseResponse })
+    const itemId = request.params.itemId;
+    const databaseResponse = await ItemModel.findByIdAndDelete(itemId); //ska hitta en vara baserat på id och sedan deleta
+    response
+      .status(200)
+      .send({ message: 'Successfully deleted item', data: databaseResponse });
   } catch (error) {
     response.status(500).send({
-      message: `Error while trying to delete item with ID ${itemId}`
-    })
+      message: `Error while trying to delete item with ID ${itemId}`,
+    });
   }
-}
+};
 
 const userLikedItem = async (request, response) => {
-  const itemId = request.body.id
-  const email = request.body.email
+  const itemId = request.body.id;
+  const email = request.body.email;
   try {
-    const databaseResponse = await ItemModel.findOneAndUpdate({ _id: itemId }, { $addToSet: { interestedUsers: email } })
+    const databaseResponse = await ItemModel.findOneAndUpdate(
+      { _id: itemId },
+      { $addToSet: { interestedUsers: email } }
+    );
     response.status(200).send({
       message: 'Liked items array was updated',
-      data: databaseResponse
-    })
+      data: databaseResponse,
+    });
   } catch (error) {
     response.status(500).send({
       message: 'Could not update liked items array',
-      stack: error
-    })
+      stack: error,
+    });
   }
-}
+};
 
 const userDislikedItem = async (request, response) => {
-  const itemId = request.body.id
-  const email = request.body.email
+  const itemId = request.body.id;
+  const email = request.body.email;
   try {
-    const databaseResponse = await ItemModel.findOneAndUpdate({ _id: itemId }, { $addToSet: { uninterestedUsers: email } })
-    response.status(200).send(data)
+    const databaseResponse = await ItemModel.findOneAndUpdate(
+      { _id: itemId },
+      { $addToSet: { uninterestedUsers: email } }
+    );
+    response.status(200).send(data);
   } catch (error) {
     response.status(500).send({
       message: 'Could not update unintrestedUsers array',
-      stack: error
-    })
+      stack: error,
+    });
   }
-}
+};
 
 const getMyItemsFromServer = async (request, response) => {
-  const userEmail = request.query.email
-  console.log('got the email of owner', userEmail)
+  const userEmail = request.query.email;
+  console.log('got the email of owner', userEmail);
   try {
-    const databaseResponse = await ItemModel.find({ owner: { $eq: userEmail } })
-    response.status(200).send(databaseResponse)
+    const databaseResponse = await ItemModel.find({
+      owner: { $eq: userEmail },
+    });
+    response.status(200).send(databaseResponse);
   } catch (error) {
     response.status(500).send({
       message: 'Could not get all uploaded items of the user',
-      stack: error
-    })
+      stack: error,
+    });
   }
-}
+};
 
 module.exports = {
   addItem,
@@ -97,5 +109,5 @@ module.exports = {
   deleteItem,
   userLikedItem,
   userDislikedItem,
-  getMyItemsFromServer
-}
+  getMyItemsFromServer,
+};
