672486f1d2cac5bd9c5d2dd172a951d2d835499c
Anna Novak <anna.c.novak@gmail.com>
2021-05-24 09:17:21 +0200

    Move routing from App to the file Routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,8 @@
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchUser } from '../actions';
-import Header from './AppHeader';
-
-const homeView = () => <h1>Homeview</h1>;
-const aboutView = () => <h1>Om</h1>;
-const signUpView = () => <h1>Sign Up</h1>;
+import AppHeader from './AppHeader';
+import { Routes } from '../routes/Routes';
 
 const App = () => {
   const dispatch = useDispatch();
@@ -16,16 +12,11 @@ const App = () => {
   }, [dispatch]);
 
   return (
-    <BrowserRouter>
-      <div>
-        <Switch>
-          <Header />
-          <Route exact path="/" component={homeView} />
-          <Route path="/om" component={aboutView} />
-          <Route path="/signup" component={signUpView} />
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <div>
+      <Routes>
+        <AppHeader />
+      </Routes>
+    </div>
   );
 };
 
