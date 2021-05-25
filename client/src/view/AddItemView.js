172dcb42e3bbacace68fd6699d046c722e775ec0
Anna Novak <anna.c.novak@gmail.com>
2021-05-25 15:05:12 +0200

    Add text to view

--- client/src/view/AddItemView.js
+++ client/src/view/AddItemView.js
@@ -1,3 +1,13 @@
 export const AddItemView = () => {
-  return <div>AddItemView</div>;
+  return (
+    <div>
+      <h1>Lägg till objekt</h1>
+      <ul>
+        <li>Lägg till bild</li>
+        <li>Kategori</li>
+        <li>Position</li>
+        <li>url till bild</li>
+      </ul>
+    </div>
+  );
 };
