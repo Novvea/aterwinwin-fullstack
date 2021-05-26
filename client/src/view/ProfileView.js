5677b5b0ed77017a953e7f09f415937bb99fb439
Anna Novak <anna.c.novak@gmail.com>
2021-05-26 19:32:27 +0200

    Add Links to views

--- client/src/view/ProfileView.js
+++ client/src/view/ProfileView.js
@@ -1,3 +1,6 @@
+import { Link } from 'react-router-dom';
+import RoutingPath from '../routes/RoutingPath';
+
 export const ProfileView = () => {
   return (
     <div>
@@ -5,6 +8,9 @@ export const ProfileView = () => {
       <ul>
         <li>
           <a href="/api/logout">Logout</a>
+        </li>
+        <li>
+          <Link to={RoutingPath.settingsView}> Settings</Link>
         </li>
       </ul>
     </div>
