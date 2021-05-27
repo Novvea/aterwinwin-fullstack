706118459c531bb48a6297616639aa19e9c5f8c2
Anna Novak <anna.c.novak@gmail.com>
2021-05-27 15:43:45 +0200

    Add list with users own items at profileview

--- client/src/view/ProfileView.js
+++ client/src/view/ProfileView.js
@@ -1,17 +1,20 @@
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { useState, useEffect } from 'react';
 import RoutingPath from '../routes/RoutingPath';
 import BackendAPIService from '../shared/api/service/BackendAPIService';
+import { modelNames } from 'mongoose';
 
 export const ProfileView = () => {
   const auth = useSelector((state) => state.auth);
+  const [itemAPIResponse, setItemAPIResponse] = useState([]);
 
   const showMyItems = async () => {
     if (auth._id) {
       console.log('we should have an owner with _id: ', auth._id);
       try {
         const itemsResponse = await BackendAPIService.getItemsByUser(auth._id);
-        console.log('itemsResponsData;', itemsResponse.data);
+        setItemAPIResponse(itemsResponse.data);
         console.log('Item were fetched from server');
       } catch (error) {
         console.log('errormessage: ', error);
@@ -20,6 +23,10 @@ export const ProfileView = () => {
       console.log('The items could not be fetched');
     }
   };
+
+  useEffect(() => {
+    console.log('apiResponse: ', itemAPIResponse);
+  }, [itemAPIResponse]);
 
   return (
     <div>
@@ -33,6 +40,11 @@ export const ProfileView = () => {
         </li>
       </ul>
       <button onClick={() => showMyItems()}>Visa mina upplagda objekt</button>
+      <ul>
+        {itemAPIResponse.map((item, index) => (
+          <li key={index}>{item.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
