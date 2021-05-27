b3cf3a3d129ac879e90a19399434858df8202947
Anna Novak <anna.c.novak@gmail.com>
2021-05-27 14:08:38 +0200

    Another try fixing blank heroku page

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ ItemRoutes.routes(app);
 if (process.env.NODE_ENV === 'production') {
   const path = require('path');
 
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '/client/build')));
 
   app.get('*', (request, response) => {
     response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
