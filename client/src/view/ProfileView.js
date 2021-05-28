97cbd6bc829c557b37ba6df49a8ad88abc94c700
Anna Novak <anna.c.novak@gmail.com>
2021-05-28 13:21:27 +0200

    Show items automatically, remove button

--- client/src/view/ProfileView.js
+++ client/src/view/ProfileView.js
@@ -3,30 +3,29 @@ import { useSelector } from 'react-redux';
 import { useState, useEffect } from 'react';
 import RoutingPath from '../routes/RoutingPath';
 import BackendAPIService from '../shared/api/service/BackendAPIService';
-import { modelNames } from 'mongoose';
 
 export const ProfileView = () => {
   const auth = useSelector((state) => state.auth);
   const [itemAPIResponse, setItemAPIResponse] = useState([]);
 
-  const showMyItems = async () => {
-    if (auth._id) {
-      console.log('we should have an owner with _id: ', auth._id);
-      try {
-        const itemsResponse = await BackendAPIService.getItemsByUser(auth._id);
-        setItemAPIResponse(itemsResponse.data);
-        console.log('Item were fetched from server');
-      } catch (error) {
-        console.log('errormessage: ', error);
-      }
-    } else {
-      console.log('The items could not be fetched');
-    }
-  };
-
   useEffect(() => {
-    console.log('apiResponse: ', itemAPIResponse);
-  }, [itemAPIResponse]);
+    async function showMyItems() {
+      if (auth._id) {
+        try {
+          const itemsResponse = await BackendAPIService.getItemsByUser(
+            auth._id
+          );
+          setItemAPIResponse(itemsResponse.data);
+          console.log('My items were fetched from server');
+        } catch (error) {
+          console.log('errormessage: ', error);
+        }
+      } else {
+        console.log('The items could not be fetched');
+      }
+    }
+    showMyItems();
+  }, []);
 
   return (
     <div>
@@ -39,7 +38,6 @@ export const ProfileView = () => {
           <Link to={RoutingPath.settingsView}> Settings</Link>
         </li>
       </ul>
-      <button onClick={() => showMyItems()}>Visa mina upplagda objekt</button>
       <ul>
         {itemAPIResponse.map((item, index) => (
           <li key={index}>{item.name}</li>
