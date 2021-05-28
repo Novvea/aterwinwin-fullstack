a39283ba0964fcacf324c175d800eb2c8c9d977b
Anna Novak <anna.c.novak@gmail.com>
2021-05-28 13:19:43 +0200

    Show items on homeview

--- client/src/view/HomeView.js
+++ client/src/view/HomeView.js
@@ -1,10 +1,34 @@
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useState, useEffect } from 'react';
 import RoutingPath from '../routes/RoutingPath';
+import BackendAPIService from '../shared/api/service/BackendAPIService';
 
 export const HomeView = () => {
+  const auth = useSelector((state) => state.auth);
+  const [itemAPIResponse, setItemAPIResponse] = useState([]);
+
+  useEffect(() => {
+    async function showAllItems() {
+      try {
+        const itemsResponse = await BackendAPIService.getAllItems(auth._id);
+        setItemAPIResponse(itemsResponse.data);
+        console.log('Items for the user were fetched from server');
+      } catch (error) {
+        console.log('errormessage: ', error);
+      }
+    }
+    showAllItems();
+  }, [auth]);
+
   return (
     <div>
       HomeView
+      <ul>
+        {itemAPIResponse.map((item, index) => (
+          <li key={index}>{item.name}</li>
+        ))}
+      </ul>
       <ul>
         <li>
           <Link to={RoutingPath.addItemView}>Lägg till objekt</Link>
