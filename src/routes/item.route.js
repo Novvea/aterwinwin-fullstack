66734c9438fcbeaad12e2e57689f946af3db51cd
Anna Novak <anna.c.novak@gmail.com>
2021-05-28 13:22:08 +0200

    Rename route

--- src/routes/item.route.js
+++ src/routes/item.route.js
@@ -5,7 +5,7 @@ const ItemController = require('../controllers/Item.controller');
 const routes = (app) => {
   //här talar vi om vilken metod det är, app för att nå express i Server.js
   app.post('/item', ItemController.addItem);
-  app.get('/item', ItemController.getAllItems);
+  app.get('/item', ItemController.getItems);
   app.delete('/item/:itemId', ItemController.deleteItem); //:itemId pga params
   app.patch('/item/liked', ItemController.userLikedItem);
   app.patch('/item/disliked', ItemController.userDislikedItem);
