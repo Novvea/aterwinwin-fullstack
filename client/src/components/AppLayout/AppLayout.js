f8c2220e530d368d16620957ce2fccf38b5664d8
Anna Novak <anna.c.novak@gmail.com>
2021-06-01 20:34:54 +0200

    Temporarily remove app header

--- client/src/components/AppLayout/AppLayout.js
+++ client/src/components/AppLayout/AppLayout.js
@@ -1,10 +1,9 @@
-import AppHeader from '../AppHeader/AppHeader';
+//import AppHeader from '../AppHeader/AppHeader';
 import AppFooter from '../AppFooter/AppFooter';
 import styles from './AppLayout.module.css';
 
 export const AppLayout = ({ children }) => (
   <div className={styles.layout}>
-    <AppHeader />
     <main className={styles.content}>{children}</main>
     <AppFooter />
   </div>
