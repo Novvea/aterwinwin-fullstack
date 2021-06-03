4c867a81ce3c0dacb15189009fcd80f318686f58
Anna Novak <anna.c.novak@gmail.com>
2021-06-03 13:31:10 +0200

    Add route get items

--- src/routes/item.route.js
+++ src/routes/item.route.js
@@ -1,11 +1,10 @@
-//till vilken url gör vi anropet?
-//hur anropas metoden?
 const ItemController = require('../controllers/Item.controller');
 
 const routes = (app) => {
   //här talar vi om vilken metod det är, app för att nå express i Server.js
   app.post('/item', ItemController.addItem);
   app.get('/item', ItemController.getItems);
+  app.get('/item/liked', ItemController.getLikedItems);
   app.delete('/item/:itemId', ItemController.deleteItem); //:itemId pga params
   app.patch('/item/liked', ItemController.userLikedItem);
   app.patch('/item/disliked', ItemController.userDislikedItem);
