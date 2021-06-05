1fae9fd1a465ad666de466c7dfeb2d042a724cd9
Anna Novak <anna.c.novak@gmail.com>
2021-06-05 13:23:48 +0200

    Remove console log

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -2,7 +2,6 @@ import { FETCH_USER } from '../actions/types';
 
 const authReducer = (state = null, action) => {
   //initial state is null
-  console.log('action: ', action);
   switch (
     action.type //based on the name of the action, it will do something for us
   ) {
