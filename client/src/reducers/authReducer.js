6015e34673a5440d900660d712ab9c396639fc8e
Anna Novak <anna.c.novak@gmail.com>
2021-06-05 13:53:48 +0200

    Add request status to redux stores

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,15 +1,32 @@
-import { FETCH_USER } from '../actions/types';
+import { combineReducers } from 'redux';
 
-const authReducer = (state = null, action) => {
+import {
+  FETCH_USER_REQUEST,
+  FETCH_USER_SUCCESS,
+  FETCH_USER_FAILURE,
+} from '../actions/types';
+
+const dataReducer = (state = null, action) => {
   //initial state is null
   switch (
     action.type //based on the name of the action, it will do something for us
   ) {
-    case FETCH_USER:
-      return action.payload || false;
+    case FETCH_USER_SUCCESS:
+      return action.data;
     default:
       return state;
   }
 };
 
-export default authReducer;
+const requestReducer = (state = null, action) => {
+  switch (action.type) {
+    case FETCH_USER_REQUEST:
+    case FETCH_USER_SUCCESS:
+    case FETCH_USER_FAILURE:
+      return { ...state, [action.pathname]: action.request };
+    default:
+      return state;
+  }
+};
+
+export default combineReducers({ data: dataReducer, request: requestReducer });
