25cda98575f085b20d97025cc3475b9e19d3226c
Anna Novak <anna.c.novak@gmail.com>
2021-06-06 19:53:18 +0200

    Add placeholder content

--- client/src/view/AboutView.js
+++ client/src/view/AboutView.js
@@ -2,7 +2,25 @@ import AppLayout from '../components/AppLayout/AppLayout';
 
 export const AboutView = () => (
   <AppLayout>
-    <div>About</div>
+    <h2>Om Återwin-win</h2>
+    <p>
+      Your old crap can be exactly what somebody else wishes for and vice versa,
+      so let´s get those things moving and all of you can be återvinners!{' '}
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce faucibus
+      diam at tortor scelerisque, viverra eleifend leo porta. Florentin qualiste
+      domingo ad est.
+    </p>
+    <p>
+      Adiste pharetra enim sit amet mauris luctus. Nullam elementum odio vitae
+      magna commodo tempor. Quisque tempor sapien cursus ligula dignissim.
+    </p>
+    <p>
+      Etiam ornare nisi luctus mauris interdum, eget vulputate nunc dictum.
+      Proin risus quam, luctus porttitor bibendum et, consectetur vel dui. Justo
+      ut arcu, at euismod purus sodales.
+    </p>
   </AppLayout>
 );
 
--- client/src/view/LicenseView.js
+++ client/src/view/LicenseView.js
@@ -2,7 +2,21 @@ import AppLayout from '../components/AppLayout/AppLayout';
 
 export const LicenseView = () => (
   <AppLayout>
-    <div>License</div>
+    <h2>Användarvillkor</h2>
+    <p>
+      Lorem ipsum dolor sit amet. Adiste pharetra enim sit amet mauris luctus.
+      Nullam elementum odio vitae magna commodo tempor. Quisque tempor sapien
+      cursus ligula dignissim.
+    </p>
+    <p>
+      Consectetur adipiscing elit. Fusce faucibus diam at tortor scelerisque,
+      viverra eleifend leo porta. Florentin qualiste domingo ad est.
+    </p>
+    <p>
+      Nunc id nisl pulvinar, posuere felis vulputate, convallis turpis. Id augue
+      ac lorem eleifend tincidunt at at ante. In efficitur arcu eget nulla
+      venenatis.
+    </p>
   </AppLayout>
 );
 
