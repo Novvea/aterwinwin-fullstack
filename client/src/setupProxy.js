894124039794a3676ebbdfa093866c916c2d3e52
Anna Novak <anna.c.novak@gmail.com>
2021-06-06 20:48:17 +0200

    Temporarily remove proxy middleware

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,13 +1,3 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
 module.exports = function (app) {
-  if (process.env.NODE_ENV === 'development') {
-    return app.use(
-      ['/api', '/auth/google'],
-      createProxyMiddleware({
-        target: 'http://localhost:5000',
-      })
-    );
-  }
-  return app.use(['/api', '/auth/google']);
+  app.use(['/api', '/auth/google']);
 };
